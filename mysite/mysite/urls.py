f2a6acf13d20bd45cfec949aa1847d3c2eb291b9
Mohammed <mms.dala@gmail.com>
2020-05-13 20:33:56 -0700

    changed the navbar as a drop down and changed the fill cards fields

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from cards import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view, name='home'),
+    path('memorise/', views.home_view, name='home'),
     path('fill/', views.fill_cards, name='fill_cards'),
 ]
