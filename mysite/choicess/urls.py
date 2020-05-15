f63214f0e6ce97b3e1477b4abc4580b5c3947ffc
Mohammed <mms.dala@gmail.com>
2020-05-15 00:09:24 -0700

    choicess view function and template

--- mysite/choicess/urls.py
+++ mysite/choicess/urls.py
@@ -4,6 +4,7 @@ from choicess import views
 
 urlpatterns = [
     path('', views.choicess_view, name='choicess_view'),
-    path('qa', views.choicess_aq, name='qa'),
+    path('qa', views.choicess_qa, name='qa'),
+    path('result', views.result_view, name='result'),
 
 ]
--- mysite/choicess/views.py
+++ mysite/choicess/views.py
@@ -5,35 +5,60 @@ from . import models
 import random
 from django.urls import reverse, reverse_lazy
 
-# Create your views here.
 id_list = []
 counter = 0
+user_answer = []
+stop = 1
 
 
-def choicess_aq(request):
+def choicess_qa(request):
+    global stop
     global counter
+    global user_answer
+    if counter >= (len(id_list) - 1):
+        stop = 0
     if request.method == 'POST':
         choice_selected = Choicess.objects.get(pk=id_list[counter])
         if counter >= (len(id_list) - 1):
             counter = 0
         else:
             counter += 1
-
         if request.POST.get('correct'):
-            print(request.POST)
+            user_answer.append(1)
+        else:
+            user_answer.append(2)
 
-    template_name = 'choicess/test.html'
-    context = {'choice': choice_selected}
+    print(user_answer, counter, len(id_list))
+    template_name = 'choicess/choicess_view.html'
+
+    context = {'choice': choice_selected,
+               'user_answer': user_answer, 'stop': stop}
+
     return render(request, template_name, context)
 
 
 def choicess_view(request):
+    global id_list
+    global user_answer
+    global stop
+    stop = 1
+    id_list = []
+    user_answer = []
     choice_set = Choicess.objects.all()
     for choice in choice_set.iterator():
-        global id_list
         id_list.append(choice.id)
     obj = Choicess.objects.all()
-    template_name = 'choicess/test.html'
+    template_name = 'choicess/choicess_view.html'
     context = {'choice': choice}
     return render(request, template_name, context)
-#
+
+
+def result_view(request):
+    obj = Choicess.objects.all()
+    template_name = 'choicess/test.html'
+    print(user_answer)
+
+    context = {'choice': obj, 'colored_card': user_answer}
+    return render(request, template_name, context)
+
+    #
